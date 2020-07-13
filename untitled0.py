48d9783841aad568be862b15f1bc42ee2baa6000
darkeggler <darkeggler@live.com>
2020-07-13 22:20:50 +0800

    S01

--- untitled0.py
+++ untitled0.py
@@ -6,8 +6,8 @@ Created on Sun Jul 12 17:24:01 2020
 """
 
 
-import matplotlib.path as mpath
+import matplotlib.pyplot as plt
 
-p = mpath.Path.unit_circle()
-
-p.unit_circle()
+fig, ax = plt.subplots()
+ax.plot([1,2,3,4],[2,3,4,1])
+fig.savefig(r"E:\notebooks\png\test.svg", format="svg")
